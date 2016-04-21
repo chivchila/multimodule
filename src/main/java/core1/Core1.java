e61e526f2f019eb03fcd9579f94c057c82f3382b
chivchila <chivchila@gmail.com>
2016-04-21 11:59:22 +0100

    first commit

---
+++ src/main/java/core1/Core1.java
@@ -0,0 +1,11 @@
+package core1;
+
+/**
+ * Created by chivchila on 21/04/2016.
+ */
+public class Core1 {
+    public static String returnHello1() {
+        return "Hello1";
+
+    }
+}
---
+++ src/main/java/core2/Core2.java
@@ -0,0 +1,11 @@
+package core2;
+
+/**
+ * Created by chivchila on 21/04/2016.
+ */
+public class Core2 {
+    public static String returnHello2() {
+        return "Hello2";
+
+    }
+}
